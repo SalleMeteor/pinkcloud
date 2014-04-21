849fdc4a77a516d273b69a9287afd9906a37ab86
MarcDP92 <marcandorra92@gmail.com>
2014-04-21 12:54:31 +0200

    Accounts Stuff and others
    Added accounts, saving and showing author of post, cleaning input after
    post.

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -9,6 +9,7 @@ Template.home.events({
   'click #add': function () {
     // template data, if any, is available in 'this'
       var fecha = moment();
-      Post.insert({name: $("#post").val(), date: fecha.toString()});
+      Post.insert({name: $("#post").val(), date: fecha.toString(), user: Meteor.user().emails[0].address});
+      document.getElementById('post').value = '';
   }
 });
