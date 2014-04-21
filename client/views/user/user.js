087086180791fce8a80464a3d59ccaa44c57c184
MarcDP92 <marcandorra92@gmail.com>
2014-04-21 18:03:39 +0200

    User section with date search

--- client/views/user/user.js
+++ client/views/user/user.js
@@ -1,10 +1,39 @@
+var clicked = false;
+
 Template.user.helpers({
   	post: function(){
-  		return Post.find({user:Meteor.user().emails[0].address}, { sort: {date: -1}});
+  		var date = Session.get("my-datepicker");
+        var stuff = Post.find({user:Meteor.user().emails[0].address}, {sort: {date: -1}});                
+        var aux = new Array();
+        var index = 0;
+        stuff.forEach(function (stf) {
+        	if(moment(stf.date).format('MM/DD/YYYY') == date || !clicked){
+	            aux[index] = {'name' : stf.name , 'date' : moment(stf.date).fromNow()};
+	            index += 1;
+        	}
+        });
+        return aux;
 	},
 });
 
 
 Template.user.events({
+	  'click #search': function () {
+	   Session.set("my-datepicker", $("#my-datepicker").val());
+	   clicked = true;
+  }
+});
 
+Template.user.rendered=function() {
+    $('#my-datepicker').datepicker({
+    todayHighlight: true
+});
+}
+
+Template.stuff.created = function() {
+    Session.set("my-datepicker", "");
+}
+
+$( "#my-datepicker" ).click(function() {
+  alert($( "#my-datepicker" ).val());
 });
