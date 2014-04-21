694aed4e35e075bffebe326ba0010dd5119a11c8
seesk <vladimir.shenkov@gmail.com>
2014-04-21 17:42:34 +0200

    Stuff search

--- client/views/stuff/stuff.js
+++ client/views/stuff/stuff.js
@@ -1,6 +1,12 @@
 Template.stuff.helpers({
 	post: function(){
-        var stuff = Post.find({}, {sort: {date: -1}, limit: 10});
+        var hs = Session.get("hashtag");
+        if(hs){
+            var stuff = Post.find({hashtag : hs}, {sort: {date: -1}, limit: 20});
+        }
+        else{
+            var stuff = Post.find({}, {sort: {date: -1}, limit: 20});                
+        }
         var aux = new Array();
         var index = 0;
         stuff.forEach(function (stf) {
@@ -13,5 +19,12 @@ Template.stuff.helpers({
 
 
 Template.stuff.events({
-
+    'click #searchbtn': function () {
+    // template data, if any, is available in 'this'
+        Session.set("hashtag", $("#hgfield").val());
+    },
 });
+
+Template.stuff.created = function() {
+    Session.set("hashtag", "");
+}
